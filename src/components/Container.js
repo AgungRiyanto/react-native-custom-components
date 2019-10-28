2786d5aab5ae7b1fc5cd107522fb1032defc965c
agung reyanto <agungreyanto@marlinbooking.id>
2019-10-28 15:15:12 +0700

    init

---
+++ src/components/Container.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+
+const Container = props => {
+    return (
+        <View style={[styles.default, props.style]}>
+            { props.children }
+        </View>
+    );
+}
+
+export default Container;
+
+const styles = StyleSheet.create({
+    default: {
+        flex: 1,
+        backgroundColor: '#fff'
+    }
+});
